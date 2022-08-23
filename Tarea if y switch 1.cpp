9b6ede4309e0338f271d90badd1227bf54432719
dyla-123 <111187632+dyla-123@users.noreply.github.com>
2022-08-23 16:58:42 -0600

    Add files via upload

---
+++ Tarea if y switch 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+main() {
+	//numero es positivo>0 o es numero negativo<0
+	int num=0;
+	cout<<" Ingrese el numero ";
+	cin>>num;
+	if (num>0) {// >0
+	    //entra a aca cuando la condicion es verdadera
+		cout<<" Positivo "<<endl;
+	}else {//<=0
+	     if (num==0){// 0
+		 
+		 
+		cout<<" Neutro "<<endl;
+	}else{// <0
+		cout<<" Negativo "<<endl;
+	}
+	 	
+	}
+	 system("pause");
+	 
+	 }
